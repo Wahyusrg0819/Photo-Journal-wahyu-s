5715d2df0ceed4d3124784d9e47a0e82ce305701
Wahyusrg0819 <fatina0819@gmail.com>
2024-06-22 02:15:52 +0700

    #1 fix small bugs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-import 'baguettebox.js/dist/baguettebox.min.css';
-import baguetteBox from 'baguettebox.js';
 import './style.css'
 import router from './router'
-import App from './App.vue'
 import { Quasar } from 'quasar'
+import App from './App.vue'
 
 import '@quasar/extras/material-icons/material-icons.css'
 // Import Quasar css
@@ -17,7 +15,6 @@ import 'quasar/src/css/index.sass'
 createApp(App)
   .use(router)
   .use(Quasar)
-  .use(baguetteBox)
   .mount('#app')
 
   
